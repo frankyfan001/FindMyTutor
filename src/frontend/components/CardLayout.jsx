55003a0a460b4766d20f09244a8c0af446bb4054
chengzhifan <frankyfan003@gmail.com>
2021-07-04 08:24:34 -0700

    Restructured HomePage and improved its styling.

--- src/frontend/components/CardLayout.jsx
+++ src/frontend/components/CardLayout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {
   Grid, IconButton, Typography, Avatar, makeStyles,
 } from '@material-ui/core';
@@ -5,45 +6,70 @@ import { red } from '@material-ui/core/colors';
 // import { AccountCircle } from '@material-ui/icons';
 import React from 'react';
 import { SchoolChip, CourseChip } from './Cards';
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 const useCardStyles = makeStyles(() => ({
   root: {
-    maxWidth: '100%',
-    background: 'linear-gradient(45deg, #F36887AE 30%, #F18651B0 90%)',
-    maxHeight: '160px',
+    width: '98.3%',
+    background: '#f6ddd1',
   },
   root1: {
-    maxWidth: '100%',
-    background: 'linear-gradient(45deg, #EED4D9FF 30%, #EAD7CFFF 90%)',
-    maxHeight: '160px',
-  },
-  root2: {
-    maxWidth: '100%',
-    background: '#eed4d9',
-    maxHeight: '160px',
-  },
-  light: {
-    maxHeight: '160px',
-    background: 'linear-gradient(45deg, #f3e5f5 50%, #8e24aa 30%)',
-  },
-  media: {
-    // height: '0',
-    paddingTop: '56.25%', // 16:9
-    float: 'left',
-    position: 'relative',
-    maxWidth: '100px',
-    maxHeight: '100px',
-  },
-  left: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    width: '98.3%',
+    background: '#fcf4ee',
   },
   avatar: {
-    backgroundColor: red[500],
+    background: null,
   },
+  nameButton: {
+    textTransform: 'none',
+  },
+  dateButton: {
+    textTransform: 'none',
+  },
+  schoolButton: {
+    textTransform: 'none',
+    minWidth: '8vh',
+    color: 'white',
+    background: '#f1af8d',
+    boxShadow: 'none',
+  },
+  courseButton: {
+    textTransform: 'none',
+    minWidth: '8vh',
+    color: 'white',
+    background: '#a5c1e2',
+    boxShadow: 'none',
+  },
+  descriptionButton: {
+    textTransform: 'none',
+  },
+  wageButton: {
+    textTransform: 'none',
+  },
+  LikesButton: {
+    textTransform: 'none',
+  },
+  // light: {
+  //   maxHeight: '160px',
+  //   background: 'linear-gradient(45deg, #f3e5f5 50%, #8e24aa 30%)',
+  // },
+  // media: {
+  //   // height: '0',
+  //   paddingTop: '56.25%', // 16:9
+  //   float: 'left',
+  //   position: 'relative',
+  //   maxWidth: '100px',
+  //   maxHeight: '100px',
+  // },
+  // left: {
+  //   display: 'flex',
+  //   justifyContent: 'space-between',
+  // },
 }));
 
-export function CardDemo({ post, idx }) {
+export default function CardLayout({ post, idx }) {
   const classes = useCardStyles();
   // style={{ background: 'palegreen' }}
   // const firstLetter = cardHooks.name[0];
@@ -51,63 +77,76 @@ export function CardDemo({ post, idx }) {
     <>
       <div>
         <Grid container spacing={2} className={idx % 2 === 0 ? classes.root : classes.root1}>
+
+          {/*Avatar*/}
           <Grid item xs={12} md={1}>
             <IconButton>
-              {/* <AccountCircle /> */}
               <Avatar aria-label="avatar" className={classes.avatar}>
-                {/* {firstLetter} */}
                 S
               </Avatar>
-              {/* UserIcon */}
             </IconButton>
           </Grid>
+
+          {/*Name & Date*/}
           <Grid item xs={12} md={2}>
             <Grid container spacing={2}>
               <Grid item xs={12} md={12}>
+                <Button size="small" className={classes.nameButton}>{ post.tutor }</Button>
                 {/* <Typography>{cardHooks.name}</Typography> */}
-                <Typography>{ post.tutor }</Typography>
+                {/*<Typography>{ post.tutor }</Typography>*/}
                 {/* UserName */}
               </Grid>
               <Grid item xs={12} md={12}>
+                <Button size="small" className={classes.dateButton}>16/06/2022</Button>
                 {/* <Typography>{cardHooks.date}</Typography> */}
-                <Typography>16/06/2022</Typography>
+                {/*<Typography>16/06/2022</Typography>*/}
                 {/* PostDate */}
               </Grid>
             </Grid>
           </Grid>
+
+          {/*School & Course*/}
           <Grid item xs={12} md={2}>
             <Grid container spacing={2}>
               <Grid item xs={12} md={12}>
+                <Button variant="contained" size="small" className={classes.schoolButton}>{post.school}</Button>
                 {/* <Button variant="outlined" size="small" className="try">UBC</Button> */}
                 {/* <SchoolChip school={cardHooks.school} /> */}
-                <SchoolChip school={post.school} />
+                {/*<SchoolChip school={post.school} />*/}
                 {/* School */}
               </Grid>
               <Grid item xs={12} md={12}>
+                <Button variant="contained" size="small" className={classes.courseButton}>{post.course}</Button>
                 {/* <Button variant="outlined" size="small" className="try">CPSC 455</Button> */}
                 {/* <CourseChip course={cardHooks.course} /> */}
-                <CourseChip course={post.course} />
+                {/*<CourseChip course={post.course} />*/}
                 {/* Course */}
               </Grid>
             </Grid>
           </Grid>
+
+          {/*Description*/}
           <Grid item xs={12} md={6}>
             <Typography>
+              <Button size="small" className={classes.descriptionButton}>{post.description}</Button>
               {/* Brief Description: this should be limited to the first 100 characters... */}
-              {post.description}
               {/* {cardHooks.description} */}
               {/* BriefDescription */}
             </Typography>
           </Grid>
+
+          {/*Wage & Likes*/}
           <Grid item xs={12} md={1}>
             <Grid container spacing={2}>
               <Grid item xs={12} md={12}>
-                <Typography>{post.price}</Typography>
+                <Button size="small">{post.price}</Button>
+                {/*<Typography>{post.price}</Typography>*/}
                 {' '}
                 {/* WagePerHour */}
               </Grid>
               <Grid item xs={12} md={12}>
-                {/* <Typography>5 v 1</Typography> */}
+                <Button size="small" startIcon={<ThumbUpIcon />}>33</Button>
+                 {/*<Typography>5</Typography>*/}
                 {/* {' '} */}
                 {/* Like vs Dislike */}
               </Grid>
@@ -119,5 +158,3 @@ export function CardDemo({ post, idx }) {
     </>
   );
 }
-
-export default CardDemo;
--- src/frontend/components/HomePage.jsx
+++ src/frontend/components/HomePage.jsx
@@ -1,26 +1,112 @@
 /* eslint-disable */
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import {CardsApp} from "./CardsApp";
+import {
+  Button,
+  Grid, makeStyles, Paper,
+} from '@material-ui/core';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import AddIcon from '@material-ui/icons/Add';
+import { mockPosts } from './mocks/mockPosts';
+
+import FilterTreeView from './FilterTree';
+import usePosts from '../hooks/usePosts';
+import CardLayout from './CardLayout';
+import { SearchInput } from './Search';
 import Banner from "./Banner";
 
-const useStyles = makeStyles((theme) => ({
-
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '80%',
+    margin: 'auto',
+  },
+  button: {
+    background: 'linear-gradient(45deg, #F36887AE 30%, #F18651B0 90%)',
+  },
 }));
+
+const usePostsCards = () => {
+  const { posts, getPosts } = usePosts();
+  const [filter, setFilter] = useState('');
+  // const [filteredPosts, setFilteredPosts] = useState(posts);
+  const [filteredPosts, setFilteredPosts] = useState(mockPosts);
+
+  // Click filter tree item
+  const handleFilterClick = (e) => {
+    e.preventDefault();
+    const item = e.target.innerText.toLowerCase().trim();
+    setFilter(item);
+  };
+
+  // filter posts based on filter tree selection
+  const handleSearch = (newValue) => {
+    const valueProcessed = newValue.trim().toLowerCase();
+    const newCards = mockPosts.filter((card) => {
+      console.log(filter);
+      if (filter) {
+        return card[filter].trim().toLowerCase().includes(valueProcessed);
+      }
+      return Object.values(card).some((val) => val.trim().toLowerCase().includes(valueProcessed));
+    });
+    setFilteredPosts(newCards);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  useEffect(() => {
+    setFilteredPosts(posts);
+  }, [posts]);
+
+  return {
+    posts, filter, filteredPosts, handleClick: handleFilterClick, handleSearch,
+  };
+};
 
 export default function HomePage({accountHook}) {
   const classes = useStyles();
+  const postsHook = usePostsCards();
 
   return (
-    <>
+    <div>
       <Banner />
       <br />
-      <Grid container alignItems="center" justify="center">
-        <Grid item align="center">
-          <CardsApp accountHook={accountHook} />
+      <Grid container spacing={3} direction="column" align="center" className={classes.root}>
+
+        {/*Search Bar*/}
+        <Grid item xs={12} md={12}>
+          <Grid container spacing={0}>
+            <Grid item xs={12} md={12}>
+              <Paper variant="outlined">
+              <FilterTreeView onNodeSelect={postsHook.handleClick} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={12}>
+              <SearchInput handleSearch={postsHook.handleSearch} />
+            </Grid>
+          </Grid>
         </Grid>
+
+        {/*New Post Button*/}
+        <Grid item xs={12} md={12} align="right">
+          <Link to={accountHook.isLogin() && accountHook.isTutor() ? '/newPost' : '/login'} style={{ textDecoration: 'none', color: 'black' }}>
+            <Button variant="contained" color="primary" className={classes.button} startIcon={<AddIcon />}>
+              New Post
+            </Button>
+          </Link>
+        </Grid>
+
+        {/*Card List*/}
+        <br />
+        {postsHook.filteredPosts.map((post, idx) => (
+          <Link to={`viewPost/${post.id}`} style={{ textDecoration: 'none', color: 'black' }}>
+            <Grid item xs={12} md={12} key={idx.toString()}>
+              <CardLayout post={post} idx={idx} />
+            </Grid>
+          </Link>
+        ))}
+
       </Grid>
-    </>
+    </div>
   );
-}
+};
