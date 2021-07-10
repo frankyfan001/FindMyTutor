d80e685ec2607fb4c3ac7b71d1795f6813311c29
chengzhifan <frankyfan003@gmail.com>
2021-07-10 02:24:43 -0700

    Done endpoint "get /posts/:postId" and done styling post section of ViewPostPage - V1.

--- src/frontend/components/ViewPostPage.jsx
+++ src/frontend/components/ViewPostPage.jsx
@@ -26,33 +26,7 @@ import FormDialog from './CommentForm';
 import useComments from '../hooks/useComment';
 import usePost from '../hooks/usePost';
 
-const useRootStyle = makeStyles(() => ({
-  root: {
-    maxWidth: '80%',
-  },
-}));
-
-const useRating = (updateFunc, updateObject) => {
-  const [isCancel, setCancel] = useState(false);
-  console.log('handling rating');
-  const handleClick = (e) => {
-    const name = e.currentTarget.id;
-    console.log('handling click');
-    console.log(updateObject[name]);
-    const rate = isCancel ? updateObject[name] - 1 : updateObject[name] + 1;
-    console.log(rate);
-    const newObject = {
-      ...updateObject,
-      [name]: rate,
-    };
-    console.log(newObject);
-    setCancel(!isCancel);
-    updateFunc(newObject);
-  };
-
-  return { handleClick, isCancel };
-};
-const usePostStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: '80%',
     margin: 'auto',
@@ -120,7 +94,7 @@ const usePostStyles = makeStyles((theme) => ({
 }));
 
 export default function ViewPostPage({ accountHook }) {
-  const classes = usePostStyles();
+  const classes = useStyles();
 
   const { post } = usePost();
 
@@ -176,7 +150,7 @@ export default function ViewPostPage({ accountHook }) {
                     </Grid>
 
                     {post.availableDays.map((available, idx) => (
-                      <Grid item key={available}>
+                      <Grid item key={idx}>
                         <Chip label={content.days[idx]} className={available ? classes.available : classes.unavailable}/>
                       </Grid>
                     ))}
@@ -256,7 +230,7 @@ export default function ViewPostPage({ accountHook }) {
         {/*Comment List*/}
         <Grid item xs={12} md={12}>
           <CommentList comments={mockPost.comments} />
-          {/* <CommentList comments={post.comments} /> */}
+           {/*<CommentList comments={post.comments} />*/}
         </Grid>
 
       </Grid>
@@ -265,61 +239,61 @@ export default function ViewPostPage({ accountHook }) {
   );
 };
 
-// export function ViewPostPage1({ accountHook }) {
-//   const classes = useRootStyle();
-//   // const [post, setSinglePost] = useState(mockPost);
-//
-//   const params = queryString.parse(useLocation().search);
-//   const { id } = params;
-//   // const { getSinglePost, updatePost } = usePosts();
-//
-//   const {
-//     post, setPost, getPost, updatePost,
-//   } = usePost();
-//
-//   const { updateComment } = useComments(post.comments);
-//   // useEffect(() => {
-//   //   setPost(getPost(id));
-//   // }, []);
-//
-//   console.log(post);
-//   const { handleClick: handlePostRatingClick } = useRating(updatePost, post);
-//   const { handleCommentRatingClick } = useRating(updateComment, post.comments);
-//   return (
-//     <>
-//       <CssBaseline />
-//       <Paper variant="outlined" className={classes.root}>
-//         <Container>
-//           <Grid container spacing={5} alignContent="center">
-//             <Grid item>
-//               <Grid container spacing={5} alignContent="center" direction="row">
-//                 <Grid item>
-//                   <PostInfo profile={post.profile} content={mockPost.content} />
-//                 </Grid>
-//               </Grid>
-//             </Grid>
-//             <Grid item>
-//               <RatingArea up={post.thumbsUp} down={post.thumbsDown} handleClick={handlePostRatingClick} />
-//             </Grid>
-//             <Grid item style={{ width: '100%' }}>
-//               <CommentList comments={post.comments} isLogin={accountHook.isLogin()} handleClick={handleCommentRatingClick} />
-//             </Grid>
-//           </Grid>
-//         </Container>
-//       </Paper>
-//     </>
-//   );
-// };
+const CommentList = ({ comments, isLogin, handleClick }) => {
+  const classes = useStyles();
+  const dialogHooks = useDialog();
+  return (
+    <Grid container alignItems="flex-end" direction="column" spacing={5}>
+      <Grid item>
+        {isLogin
+          ? (
+            <Button variant="contained" color="primary" onClick={dialogHooks.handleClickOpen}>
+              Add new comment
+            </Button>
+          )
+          : (
+            <Link to="/login" style={{ textDecoration: 'none', color: 'black' }}>
+              <Button variant="contained" color="primary">
+                Add new comment
+              </Button>
+            </Link>
+          )}
 
-const useStyles = makeStyles(() => ({
-  media: {
-    paddingTop: '56.29%',
-  },
-  root: {
-    height: '100%',
-    width: '100%',
-  },
-}));
+        <FormDialog dialogHooks={dialogHooks} />
+      </Grid>
+      <Grid item className={classes.root}>
+        <Grid container alignItems="stretch">
+          {comments.map((comment, idx) => (
+            <Grid item key={idx.toString()} xs={12} style={{ paddingLeft: 0, paddingRight: 0 }}>
+              <Comment comment={comment} handleClick={handleClick} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+const useRating = (updateFunc, updateObject) => {
+  const [isCancel, setCancel] = useState(false);
+  console.log('handling rating');
+  const handleClick = (e) => {
+    const name = e.currentTarget.id;
+    console.log('handling click');
+    console.log(updateObject[name]);
+    const rate = isCancel ? updateObject[name] - 1 : updateObject[name] + 1;
+    console.log(rate);
+    const newObject = {
+      ...updateObject,
+      [name]: rate,
+    };
+    console.log(newObject);
+    setCancel(!isCancel);
+    updateFunc(newObject);
+  };
+
+  return { handleClick, isCancel };
+};
 
 const Profile = ({ profile }) => {
   const classes = useStyles();
@@ -327,7 +301,7 @@ const Profile = ({ profile }) => {
     <Grid container alignItems="space-around" className={classes.root}>
       <Paper variant="outlined" style={{ width: '100%' }}>
         <Grid item xs={6} className={classes.root}>
-          <CardMedia className={classes.media} image={profile.img} title="profile img" />
+          <CardMedia image={profile.img} title="profile img" />
         </Grid>
         <Grid item xs={6}>
           <Typography variat="body2" color="textSecondary" component="p">
@@ -389,43 +363,8 @@ const useDialog = () => {
   };
 };
 
-const CommentList = ({ comments, isLogin, handleClick }) => {
-  const classes = useStyles();
-  const dialogHooks = useDialog();
-  return (
-    <Grid container alignItems="flex-end" direction="column" spacing={5}>
-      <Grid item>
-        {isLogin
-          ? (
-            <Button variant="contained" color="primary" onClick={dialogHooks.handleClickOpen}>
-              Add new comment
-            </Button>
-          )
-          : (
-            <Link to="/login" style={{ textDecoration: 'none', color: 'black' }}>
-              <Button variant="contained" color="primary">
-                Add new comment
-              </Button>
-            </Link>
-          )}
-
-        <FormDialog dialogHooks={dialogHooks} />
-      </Grid>
-      <Grid item className={classes.root}>
-        <Grid container alignItems="stretch">
-          {comments.map((comment, idx) => (
-            <Grid item key={idx.toString()} xs={12} style={{ paddingLeft: 0, paddingRight: 0 }}>
-              <Comment comment={comment} handleClick={handleClick} />
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-    </Grid>
-  );
-};
-
 const Comment = ({ comment, handleClick }) => (
-  <Grid container wrap="nowrap" spacing={2} xs={12} alignContent="space-between">
+  <Grid container wrap="nowrap" spacing={2} alignContent="space-between">
     <Grid item xs={1}>
       <Avatar alt={comment.user.name} src={comment.user.img} />
     </Grid>
@@ -442,28 +381,29 @@ const Comment = ({ comment, handleClick }) => (
         {new Date().toLocaleDateString()}
       </p>
     </Grid>
-    <Grid item align="right" justify="space-between" xs={3}>
+    <Grid item align="right" xs={3}>
       <RatingArea up={comment.thumbsUp} down={comment.thumbsDown} handleClick={handleClick} />
     </Grid>
   </Grid>
 );
 
-const useRatingStyles = ((theme) => ({
+const useRatingStyles = makeStyles((theme) => ({
   hover: {
     '&:hover': {
       backgroundColor: 'blue',
     },
   },
 }));
+
 const RatingArea = ({ up, down, handleClick }) => {
   const classes = useRatingStyles();
+
   return (
     <Grid container direction="row" spacing={3}>
       <Grid item className={classes.hover}>
         <Button
           variant="contained"
           color="secondary"
-          className={classes.button}
           startIcon={<ThumbUpIcon />}
           onClick={handleClick}
           id="thumbsUp"
@@ -475,7 +415,6 @@ const RatingArea = ({ up, down, handleClick }) => {
         <Button
           variant="contained"
           color="secondary"
-          className={classes.button}
           startIcon={<ThumbDown />}
           id="thumbsDown"
           onClick={handleClick}
