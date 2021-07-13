0f29e112ac6909a53a5c4121481f88af9e8c99cb
LDY1998 <caliudeyu2017@gmail.com>
2021-07-12 22:19:07 -0700

    fix filter bug

--- src/frontend/components/HomePage.jsx
+++ src/frontend/components/HomePage.jsx
@@ -25,6 +25,7 @@ const useStyles = makeStyles(() => ({
 }));
 
 const usePostsCards = () => {
+  const filters = ['tutor', 'school', 'thumbup', 'course'];
   const { posts } = usePosts();
   const [filter, setFilter] = useState('');
   // const [filteredPosts, setFilteredPosts] = useState(posts);
@@ -42,10 +43,14 @@ const usePostsCards = () => {
     const valueProcessed = newValue.trim().toLowerCase();
     const newCards = mockPosts.filter((card) => {
       console.log(filter);
-      if (filter) {
+      if (filter && filters.includes(filter)) {
         return card[filter].trim().toLowerCase().includes(valueProcessed);
       }
-      return Object.values(card).some((val) => val.trim().toLowerCase().includes(valueProcessed));
+      console.log(Object.values(card));
+      return Object.values(card).some((val) => {
+        console.log(val);
+        return val.toString().trim().toLowerCase().includes(valueProcessed);
+      });
     });
     setFilteredPosts(newCards);
   };
